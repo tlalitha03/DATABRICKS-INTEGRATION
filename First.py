96d00e0c7cb991351b442f1f379f16bc4b876ece
tlalitha03 <thakkasilalalitha@gmail.com>
2026-03-31 08:58:00 +0000

    second commit

---
+++ First.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+print("Hello World...!!!")
+
+# COMMAND ----------
+
+print("This is my First Databricks Notebook...")
